ab124a8b292fc03a6ee8c68664607fc3ad41a41b
Metin OSMAN <mosman@mixdata.com>
2020-06-02 02:54:56 +0200

    Remove server part and add environment configurations

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,45 +6,62 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import {Button} from '@material-ui/core';
+import Grid from "@material-ui/core/Grid";
 
 class SimpleTable extends React.Component {
 
-  state = {
-    data: []
-  };
+    state = {
+        data: []
+    };
 
-  componentDidMount() {
-    fetch('http://localhost:4001/players/all')
-      .then((response) => response.json())
-      .then(data => {
-        this.setState({ data: data });
-      });
-  };
+    componentDidMount() {
+        var headers = new Headers();
+        var myInit = {headers: headers}
+        headers.append("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTU5MzUyODkzOX0.jGiDuMeCjkSeBydgypsT5n3aSJVUQE7Wdolz0fNHjdewvtw4SuMHl7VwSAqp73CcxyxMXMOKL0swKAIWC_-u9A");
+        fetch(process.env.REACT_APP_API_ENDPOINT + '/characters', myInit)
+            .then((response) => response.json())
+            .then(data => {
+                console.log(data);
+                this.setState({data: data});
+            });
+    };
 
-  render() {
-    return (
-      <TableContainer component={Paper}>
-        <Table style={{ width: '600px' }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Nom</TableCell>
-              <TableCell>Race</TableCell>
-              <TableCell>Profession</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.data.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell>{row.name}</TableCell>
-                <TableCell>{row.race}</TableCell>
-                <TableCell>{row.profession}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
-  }
+    handleClick() {
+
+    }
+
+    render() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Button variant="contained" onClick={this.handleClick}>Ajouter</Button>
+                </Grid>
+                <Grid>
+                    <TableContainer component={Paper}>
+                        <Table style={{width: '600px'}} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Nom</TableCell>
+                                    <TableCell>Race</TableCell>
+                                    <TableCell>Profession</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {this.state.data.map((row) => (
+                                    <TableRow key={row.name}>
+                                        <TableCell>{row.name}</TableCell>
+                                        <TableCell>{row.race != null ? row.race.name : ""}</TableCell>
+                                        <TableCell>{row.profession != null ? row.profession.name : ""}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+        )
+    }
 }
 
 export default SimpleTable;
